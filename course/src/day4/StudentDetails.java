2c6e2076f2744bef1e7994a631ea398fdeac04ae
Team-thedatatribune <mail@thedatatribune.com>
2021-07-15 22:17:49 +0530

    day4 init

---
+++ course/src/day4/StudentDetails.java
@@ -0,0 +1,30 @@
+package day4;
+
+import java.util.*;
+
+class supportClass {
+	// Attributes
+
+
+	// constructor(s)
+	
+	
+	// setters
+	
+	
+	// getters
+	
+	
+	// Output
+	public void opFun() {
+		System.out.println("something");
+	}
+}
+
+public class StudentDetails {
+
+	public static void main(String[] args) {
+		// TODO workflow
+	}
+
+}
